4360467467a631843e23e2176b14550e11800690
Thomas Hebb <tommyhebb@gmail.com>
2012-03-08 18:33:42 -0500

    Added comments and fixed a number of minor bugs

--- main.c
+++ main.c
@@ -23,8 +23,8 @@
 #include <signal.h>
 #include <errno.h>
 
-#define MAX_PASSES 5
-#define MAX_LENGTH 100
+#define MAX_PASSES 5	// Maximum number of passes to attempt before failing.
+#define MAX_LENGTH 100	// Maximum string length to guess.
 
 /* usecdifference - Find a sub-second difference between microsecond values.
  * Parameters:
@@ -35,7 +35,7 @@
 inline long usecdifference (long start, long end) {
 	long result = end - start;
 	if (end < start) {
-		// Everything we time will take less than a second
+		// Everything we time will take less than a second.
 		result += 1000000;
 	}
 	return result;
@@ -43,15 +43,15 @@ inline long usecdifference (long start, long end) {
 
 /* findcorrect - Find the index of the 1 in an array of 0s.
  * Parameters:
- *   counts: Unsigned char array.
+ *   possibilities: Unsigned char array.
  *   length: Number of elements in array.
  * Return value: Either the index of the 1, -1 if there are multiple,
  *               or -2 if there are none.
  */
-int findcorrect (unsigned char *counts, int length) {
+int findcorrect (unsigned char *possibilities, int length) {
 	int i, index = -2;
 	for (i = 0; i < length; ++i) {
-		if (counts[i] == 1) {
+		if (possibilities[i] == 1) {
 			if (index != -2) {
 				return -1;
 			}
@@ -112,6 +112,8 @@ void qs_helper (long *array, int start, int end) {
  *   length: Number of elements in array.
  */
 void quicksort (long *array, int length) {
+	// See http://en.wikipedia.org/wiki/Quicksort
+	// for algorithm documentation.
 	qs_helper(array, 0, length - 1);
 }
 
@@ -125,16 +127,18 @@ void quicksort (long *array, int length) {
  * Return value: PID of child process.
  */
 pid_t startguesser (int fd[3], char *path, char *file, char *str) {
+	// Create pipes for stdin, stdout, and stderr of the guesser.
 	int infd[2], outfd[2], errfd[2];
-	pid_t child;
-	// Create pipes
 	if (pipe(infd) == -1 || pipe(outfd) == -1 || pipe(errfd) == -1) {
 		perror("Error creating pipes");
 		exit(EXIT_FAILURE);
 	}
 
+	// Spawn the child process.
+	pid_t child;
 	if (!(child = fork())) {
-		// Child
+		// We're the child.
+		// Connect the pipes to standard file descriptors and close unneeded ones.
 		dup2(infd[0], 0);
 		  close(infd[0]);
 		  close(infd[1]);
@@ -144,6 +148,8 @@ pid_t startguesser (int fd[3], char *path, char *file, char *str) {
 		dup2(errfd[1], 2);
 		  close(errfd[0]);
 		  close(errfd[1]);
+
+		// Start the guesser.
 		if (execl(path, path, file, str, NULL) == -1) {
 			perror("Error executing guesser");
 			exit(EXIT_FAILURE);
@@ -151,19 +157,21 @@ pid_t startguesser (int fd[3], char *path, char *file, char *str) {
 		exit(EXIT_SUCCESS);
 	}
 
-	// Parent
 	if (child == -1) {
 		perror("Error forking");
 		exit(EXIT_FAILURE);
 	}
 
-	// Set pipe array and return
+	// We're the parent.
+	// Add the right file descriptors to the array and close the others.
 	fd[0] = infd[1];
 	  close(infd[0]);
 	fd[1] = outfd[0];
 	  close(outfd[1]);
 	fd[2] = errfd[0];
 	  close(errfd[1]);
+
+	// Return the child's PID.
 	return child;
 }
 
@@ -173,31 +181,32 @@ pid_t startguesser (int fd[3], char *path, char *file, char *str) {
  *   path: Path of target application.
  *   file: File to target.
  *   str: String to guess.
- *   wrongind: Pointer to contain the index of the first wrong character.
+ *   firstwrong: Pointer to contain the index of the first wrong character.
  * Return value: Time taken for the second-to-last character to be checked or
  *               -1 if the string is too short.
  */
-long teststring (char *path, char *file, char *str, int *wrongind) {
+long teststring (char *path, char *file, char *str, int *firstwrong) {
+	// We cannot calculate time differences if the string is too short.
 	int numchars = strlen(str);
 	if (numchars <= 1) {
 		return -1;
 	}
 
-	int fd[3], i, highval = 0, delta;
-	struct timeval temp;
-	long times[numchars - 1];
-	FILE *errfile;
-	char current;
-
-	// Start the guesser and open the stderr fd
+	// Start the password guesser and open stderr.
+	int fd[3];
 	startguesser(fd, path, file, str);
-	close(fd[0]);
-	close(fd[1]);
-	errfile = fdopen(fd[2], "r");
+	  close(fd[0]);
+	  close(fd[1]);
+	FILE *errfile = fdopen(fd[2], "r");
+
+	// Make sure buffering is disabled so we can accurately time the dots.
 	setvbuf(errfile, NULL, _IONBF, 0);
 
-	// Time each '.' output
-	i = 0;
+	// Time each '.' output and store the timestamps in times.
+	int i = 0;
+	struct timeval temp;
+	char current;
+	long times[numchars - 1];
 	while ((current = fgetc(errfile)) != EOF) {
 		if (current == '.') {
 			gettimeofday(&temp, 0);
@@ -206,13 +215,18 @@ long teststring (char *path, char *file, char *str, int *wrongind) {
 		}
 	}
 
+	// Calculate the time differences and store the index of the highest
+	// in firstwrong so we can backtrack later.
+	int highval = 0, delta;
 	for (i = 1; i < numchars; ++i) {
 		delta = usecdifference(times[i-1], times[i]);
 		if (delta > highval) {
 			highval = delta;
-			*wrongind = i - 1;
+			*firstwrong = i - 1;
 		}
 	}
+
+	// Clean up and return the delta of the last two dots.
 	fclose(errfile);
 	return delta;
 }
@@ -225,16 +239,16 @@ long teststring (char *path, char *file, char *str, int *wrongind) {
  * Return value: 0 if the string is not correct, 1 if it is.
  */
 unsigned char checkstring (char *path, char *file, char *str) {
+	// Start the password guesser and open stderr.
 	int fd[3];
-	FILE *errfile;
-	char current;
-
-	// Start the guesser and open the stderr fd
 	startguesser(fd, path, file, str);
-	close(fd[0]);
-	close(fd[1]);
-	errfile = fdopen(fd[2], "r");
+	  close(fd[0]);
+	  close(fd[1]);
+	FILE *errfile = fdopen(fd[2], "r");
 
+	// Count the number of characters on the third line of stderr.
+	// The password is correct if the line is not empty.
+	char current;
 	unsigned char linecount = 0;
 	while ((current = fgetc(errfile)) != EOF) {
 		if (current == '\n' && ++linecount == 2) {
@@ -243,6 +257,8 @@ unsigned char checkstring (char *path, char *file, char *str) {
 			return current != EOF;
 		}
 	}
+
+	// If we're here, the program output was unexpected.
 	fclose(errfile);
 	return 0;
 }
@@ -250,14 +266,17 @@ unsigned char checkstring (char *path, char *file, char *str) {
 /* variance - calculates the variance of an array of values.
  * Parameters:
  *   data: Array to calculate the variance of.
- *   variation: Array to store the cumulative variances in.
+ *   variances: Array to store the cumulative variances in.
  *   length: Number of elements in arrays.
  * Return value: The calculated variance.
  */
-float variance (long *data, float *variation, int length) {
+float variance (long *data, float *variances, int length) {
+	// See http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#On-line_algorithm
+	// for algorithm documentation.
 	if (length == 0) {
 		return 0;
 	}
+
 	int i, n = 0;
 	float delta, mean = 0, M2 = 0;
 	for (i = 0; i < length; ++i) {
@@ -267,7 +286,7 @@ float variance (long *data, float *variation, int length) {
 		if (n > 1) {
 			M2 += delta * (data[i] - mean);
 		}
-		variation[i] = M2 / n;
+		variances[i] = M2 / n;
 	}
 	return M2 / n;
 }
@@ -280,17 +299,21 @@ float variance (long *data, float *variation, int length) {
  * Return value: The calculated threshhold.
  */
 int findthreshhold (long *in, int length) {
+	// Calculate the cumulative variance for each input element.
+	float variances[length];
+	variance(in, variances, length);
+
+	// Find the first local maximum variance.
 	int i = 1;
 	float dold, dnew;
-	float variation[length];
-	variance(in, variation, length);
-
-	dnew = variation[1] - variation[0];
+	dnew = variances[1] - variances[0];
 	do {
 		++i;
 		dold = dnew;
-		dnew = variation[i] - variation[i - 1];
+		dnew = variances[i] - variances[i - 1];
 	} while (dnew >= dold);
+
+	// Return the value at the first local maximum as the threshhold.
 	return in[i - 1];
 }
 
@@ -301,23 +324,27 @@ int findthreshhold (long *in, int length) {
  *   length: Number of elements in arrays.
  */
 void markoutliers (long *in, unsigned char *out, int length) {
-	int i, threshhold;
+	// Sort the input array for processing.
 	long sorted[length];
 	memcpy(sorted, in, length * sizeof(long));
 	quicksort(sorted, length);
 
-	threshhold = findthreshhold(sorted, length);
-	/*printf("    {");
-	for (i = 0; i < length - 1; ++i) {
-		printf("%i, ", sorted[i]);
-	}
-	printf("%i} = %i\n", sorted[i], threshhold);*/
+	// Find the threshhold that seperates correct values from incorrect ones.
+	int threshhold = findthreshhold(sorted, length);
 
+	// Mark values accordingly in the output array.
+	int i;
 	for (i = 0; i < length; ++i) {
 		if (in[i] >= threshhold) {
 			out[i] = 0;
 		}
 	}
+
+	/*printf("    {");
+	for (i = 0; i < length - 1; ++i) {
+		printf("%i, ", sorted[i]);
+	}
+	printf("%i} = %i\n", sorted[i], threshhold);*/
 }
 
 /* guesschar - Guesses the next character in string known.
@@ -330,52 +357,81 @@ void markoutliers (long *in, unsigned char *out, int length) {
  *               string is incorrect.
  */
 char guesschar (char *path, char *file, char *known, char *charlist) {
+	// Calculate the number of possible characters
+	// and the position that's being guessed.
 	int numchars = strlen(charlist);
 	int index = strlen(known);
-	long times[numchars];
-	unsigned char counts[numchars];
 
+	// Arrays that will store the raw timing data
+	// and processed possibility data.
+	long times[numchars];
+	unsigned char possibilities[numchars];
+
+	// Add our test character and null terminator.
 	known[index + 1] = '!';
 	known[index + 2] = '\0';
 
 	// printf("Guessing character %i\n", index + 1);
-	// printf("  Initializing counts\n");
-	int i, j = 0;
+	// printf("  Initializing possibilities\n");
+
+	// At the beginning, every character is a possibility.
+	int i;
 	for (i = 0; i < numchars; ++i) {
-		counts[i] = 1;
+		possibilities[i] = 1;
 	}
 
 	// printf("  Running passes\n");
-	int highind, wrongind, incorrect;
-	while ((highind = findcorrect(counts, numchars)) < 0 && j <= MAX_PASSES) {
-		// printf("    Pass %i\n", j + 1);
-		incorrect = 1;
+
+	// Perform passes until only one possibility is left or the limit is reached.
+	int correct, firstwrong, isincorrect;
+	int pass = 0;
+	while ((correct = findcorrect(possibilities, numchars)) < 0 && pass <= MAX_PASSES) {
+		// printf("    Pass %i\n", pass + 1);
+
+		// Assume the current string is incorrect until a guess proves otherwise.
+		isincorrect = 1;
+
+		// Perform a guess for each character that is still a possibility.
 		for (i = 0; i < numchars; ++i) {
-			if (counts[i]) {
+			if (possibilities[i]) {
 				known[index] = charlist[i];
-				times[i] = teststring(path, file, known, &wrongind);
-				if (wrongind == index) {
-					incorrect = 0;
+
+				// Save the time into the times array for processing.
+				times[i] = teststring(path, file, known, &firstwrong);
+
+				// If the first wrong character is the same as the one
+				// we're guessing, the current string must be correct.
+				if (firstwrong == index) {
+					isincorrect = 0;
 				}
 			}
 		}
-		markoutliers(times, counts, numchars);
-		if (incorrect) {
+
+		// Backtrack if the current string is incorrect.
+		if (isincorrect) {
 			// printf("  Backtracking...\n");
 			known[index - 1] = '\0';
 			return -1;
 		}
-		++j;
-	}
-	// printf("Guessed character %i as %c after %i %s\n", index + 1, charlist[highind], j, j == 1 ? "pass" : "passes");
 
-	if (highind == -1) {
+		// Call markoutliers to perform processing on the times and store the
+		// results in possibilities.
+		markoutliers(times, possibilities, numchars);
+
+		// Increment the pass counter.
+		++pass;
+	}
+	// printf("Guessed character %i as %c after %i %s\n", index + 1, charlist[correct], pass, pass == 1 ? "pass" : "passes");
+
+	if (correct < 0) {
+		// If no correct character was found, reset the string and fail.
 		known[index] = '\0';
 		return 0;
 	} else {
+		// Otherwise, append the correct character to the string and return it.
 		known[index + 1] = '\0';
-		known[index] = charlist[highind];
-		return charlist[highind];
+		known[index] = charlist[correct];
+		return charlist[correct];
 	}
 }
 
@@ -387,20 +443,30 @@ char guesschar (char *path, char *file, char *known, char *charlist) {
  * Return value: Guessed string or 0 if an error occured.
  */
 char *findstring (char *path, char *file, char *charlist) {
+	// Allocate the memory for the string and make it empty to start.
 	char *str = (char*)malloc(MAX_LENGTH);
-	char current;
 	str[0] = '\0';
-	int i = 0;
-	while ((current = guesschar(path, file, str, charlist)) != 0 && i < MAX_LENGTH - 2) {
+	int length = 0;
+
+	// Repeatedly call guesschar to find the string. We must add 2 characters to the string
+	// length to account for the "test character" that is needed and the null terminator.
+	char current;
+	while ((current = guesschar(path, file, str, charlist)) != 0 && length < MAX_LENGTH - 2) {
+		// If the string is correct, return it.
 		if (checkstring(path, file, str)) {
 			return str;
 		}
+
+		// If the return value of guesschar is -1, we have guessed
+		// a character wrong and must backtrack.
 		if (current == -1) {
-			--i;
+			--length;
 		} else {
-			++i;
+			++length;
 		}
 	}
+
+	// We have hit the length limit and still not found the string.
 	return 0;
 }
 
@@ -409,25 +475,33 @@ char *findstring (char *path, char *file, char *charlist) {
  * Return value: You should know this.
  */
 int main (int argc, char **argv) {
+	// List of possible characters for each position.
 	char *charlist = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	FILE *fd;
 
+	// Check arguments.
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s path file\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
+	// Make sure file exists - we may not have read permission, so only warn.
+	FILE *fd;
 	if (!(fd = fopen(argv[2], "r"))) {
 		perror("Warning: Could not open file");
 	} else {
 		fclose(fd);
 	}
 
+	// Ignore return values of children so they don't become zombies.
 	signal(SIGCHLD, SIG_IGN);
+
+	// Call findstring with the target binary, target file, and character list.
+	// and print the result
 	char *str = findstring(argv[1], argv[2], charlist);
 	printf("%s\n", str == 0 ? "Sorry, the password could not be found" : str);
 	free(str);
 
+	// If we haven't failed by here, we've finished successfully.
 	return EXIT_SUCCESS;
 }
 
